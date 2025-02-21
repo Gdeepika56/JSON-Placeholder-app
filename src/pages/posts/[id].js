a9cacb919523de043577e9d9f3c1cab8c716d5b0
Gdeepika56 <dg1201@usnh.edu>
2025-02-20 21:02:45 -0500

    Add id link for post details

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -7,8 +7,24 @@ function PostDetail() {
     const { id } = router.query;
     const [post, setPost] = useState({});
 
-    
+    useEffect(() => {
+        if (id) {
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+                .then(response => setPost(response.data))
+                .catch(error => console.error(error));
+        }
+    }, [id]);
 
+    console.log(post);
+
+    if (!post) return <p>Loading.....</p>;
+
+    return (
+        <div className="container">
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    );
 }
 
 export default PostDetail;
